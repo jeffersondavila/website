7cb43a2bb34cfb6796b441d5d844ee96525ecfd0
jeffersondavila <jeffersondavila16@gmail.com>
2021-07-02 20:43:51 -0600

    Contacto finalizada

--- main.js
+++ main.js
@@ -9,7 +9,7 @@
     });
 
     d.addEventListener('click', e => {
-        if(!e.target.matches('.menu a')) return false;
+        if (!e.target.matches('.menu a')) return false;
 
         $btnMenu.firstElementChild.classList.remove('none');
         $btnMenu.lastElementChild.classList.add('none');
@@ -17,3 +17,40 @@
     });
 })(document);
 //Fin Menu
+
+//Formulario
+((d) => {
+    const $form = d.querySelector(".contact-form"),
+        $loader = d.querySelector(".contact-form-loader"),
+        $response = d.querySelector(".modal-contact-response");
+
+    $form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        $loader.classList.remove("none");
+        fetch("https://formsubmit.co/ajax/jeffersondavila16@gmail.com", {
+            method: "POST",
+            body: new FormData(e.target),
+        })
+            .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+            .then((json) => {
+                console.log(json);
+                location.hash = "#gracias";
+                $form.reset();
+            })
+            .catch((err) => {
+                console.log(err);
+                let message =
+                    err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+                $response.querySelector(
+                    "h3"
+                ).innerHTML = `Error ${err.status}: ${message}`;
+            })
+            .finally(() => {
+                $loader.classList.add("none");
+                setTimeout(() => {
+                    location.hash = "#close";
+                }, 3000);
+            });
+    });
+})(document);
+//Fin Formulario
